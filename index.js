8041552d593df19dbcfb0fbb2bd1923fb80f64bf
William Cruz <williamcruzacero@hotmail.com>
2023-02-16 16:52:33 -0500

    creamos registrar usuario

--- index.js
+++ index.js
@@ -2,66 +2,66 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const session = require('express-session')
-const {StatusCodes} = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 const MongoStore = require('connect-mongo')
 
 
 const conectDB = async () => {
     try {
-    await mongoose.connect('mongodb+srv://william:1234@database.vicdvt5.mongodb.net/ecommerce?retryWrites=true&w=majority', {
-        serverSelectionTimeoutMS: 60 * 1000,
-    }) 
-    console.log('Base de datos conectada');   
+        await mongoose.connect('mongodb+srv://william:1234@database.vicdvt5.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+            serverSelectionTimeoutMS: 60 * 1000,
+        })
+        console.log('Base de datos conectada');
     } catch (error) {
-    console.log('Error en la conexion a la base de datos' + error);
+        console.log('Error en la conexion a la base de datos' + error);
     }
 }
 
 const getUserModel = () => {
-    
+
     const usuarioSchema = new mongoose.Schema({
-        
+
         username: String,
         password: String,
         name: String,
         productos: [{
-            nombre: String, 
+            nombre: String,
             precio: Number,
             imagen: String,
             cantidad: Number
         }]
-      });
-      
-      return mongoose.model('usuarios', usuarioSchema);
-       
+    });
+
+    return mongoose.model('usuarios', usuarioSchema);
+
 }
 
-function verificarCampoRequerido(valor, mensaje){
+function verificarCampoRequerido(valor, mensaje) {
     if (!valor) {
         throw new Error()
     }
 }
 
-function isAuthenticated (req, res, next) {
+function isAuthenticated(req, res, next) {
     if (req.session.name) next()
     else res.redirect('/');
-  }
+}
 
 const main = async () => {
-    
+
     const app = express();
 
     await conectDB()
     const UsuarioModel = getUserModel();
-    /* const ProductoModel = getProductModel(); */
-    app.use(express.urlencoded({extended: false}));
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.json())
     app.use(session({
         secret: 'secret-key',
         resave: false,
+        rolling: true,
         saveUninitialized: false,
         cookie: {
-            maxAge: 3 * 60 * 1000
+            maxAge: 1 * 60 * 1000
         },
         store: new MongoStore({
             client: mongoose.connection.getClient(),
@@ -80,137 +80,192 @@ const main = async () => {
         res.render('formulario-inicio-sesion');
     });
 
-    app.post('/api/user', async (req, res) => {
+    app.get('/salir', (req, res) => {
+        res.render('mensaje', {mensaje: '¡Hasta luego!'})
+    })
     
-        const {username, password, name} = req.body;
-    
+    app.get('/registrarUsuario', (req, res) => {
+        res.render('formulario-registrar-usuario')
+    })
+
+    app.post('/user', async (req, res) => {
+
+        const { username, password, name } = req.body;
+
         if (!username) {
-            return res.status(StatusCodes.BAD_REQUEST).send('El nombre de usuario es requerido')
+            return res.render('error', {
+                mensaje: 'El nombre de usuario es requerido',
+                redirigir: '/registrarUsuario'
+            })
         }
-    
+
+        var isEmailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if(!isEmailRegExp.test(username)) {
+            return res.render('error', {
+                mensaje: 'El nombre de usuario debe ser un correo electrónico',
+                redirigir: '/registrarUsuario'
+            })
+        }
+
         if (!password) {
-            return res.status(StatusCodes.BAD_REQUEST).send('La contraseña es requerida')
+            return res.render('error', {
+                mensaje: 'La contraseña es requerida',
+                redirigir: '/registrarUsuario'
+            })
         }
 
         if (!name) {
-            return res.status(StatusCodes.BAD_REQUEST).send('El nombre es requerido')
+            return res.render('error', {
+                mensaje: 'El nombre es requerido',
+                redirigir: '/registrarUsuario'
+            })
         }
-    
-       const usuarioExistente = await UsuarioModel.findOne({username});
 
-        if ( usuarioExistente?.username ) {
-            return res.status(StatusCodes.BAD_REQUEST).send('El nombre de usuario no está disponible');
+        const usuarioExistente = await UsuarioModel.findOne({ username });
+
+        if (usuarioExistente?.username) {
+            return res.render('error', {
+                mensaje: 'El nombre de usuario no está disponible',
+                redirigir: '/registrarUsuario'
+            })
         }
-    
+
         const nuevoUsuario = new UsuarioModel({
             username,
             password,
             name,
             productos: []
         })
- 
+
         await nuevoUsuario.save();
-        
-        res.send(`Usuario ${username} registrado con exito`) 
+
+        res.render('mensaje', {mensaje: `Usuario ${username} registrado con exito`})
     });
 
     app.post('/login', async (req, res) => {
-        const {username, password} = req.body;
+        const { username, password } = req.body;
+        let user;
 
-        if (!username) {
-            return res.status(StatusCodes.BAD_REQUEST).send('El nombre de usuario es requerido')
-        }
+        try {
+            if (!username) {
+                throw new Error('El nombre de usuario es requerido')
+            }
     
-        if (!password) {
-            return res.status(StatusCodes.BAD_REQUEST).send('La contraseña es requerida')
-        }
+            if (!password) {
+                throw new Error('La contraseña es requerida')
+            }
     
-        const user = await UsuarioModel.findOne({username});
+            user = await UsuarioModel.findOne({ username });
+    
+            if (!user?.username) {
+                throw new Error('El usuario no esta registrado');
+            }
+    
+            if (password !== user.password) {
+                throw new Error('El nombre de usuario o contraseña es incorrecta');
+            }
 
-        if ( !user?.username ) {
-            return res.status(StatusCodes.UNAUTHORIZED).send('El usuario no esta registrado');
+        } catch (error) {
+            return res.render('error', {
+                mensaje: error.message,
+                redirigir: '/'
+            })
         }
-
-        if( password !== user.password ) {
-            return res.status(StatusCodes.UNAUTHORIZED).send('Nombre de usuario o contraseña es incorrecta')
-        } 
-
+        
         req.session.name = user.name
         req.session.username = user.username
         res.redirect('/productos')
-
     });
 
     app.post('/logout', (req, res) => {
         req.session.destroy(error => {
-            if(error){
-                res.send({error: error.message});
+            if (error) {
+                res.send({ error: error.message });
                 return;
             }
-    
-            res.redirect('/')
+            res.redirect('/salir')
         });
-    
+
     });
-    
-    app.get('/productos', isAuthenticated, async(req, res) => {
 
-        const {username, name} = req.session
+    app.get('/productos', isAuthenticated, async (req, res) => {
 
-        const user = await UsuarioModel.findOne({username});
-        
+        const { username, name } = req.session
+
+        const user = await UsuarioModel.findOne({ username });
+
         res.render('formulario-productos', {
             productos: user.productos,
             usuario: {
                 nombre: name
-            } 
+            }
         });
     });
 
     app.post('/productos', isAuthenticated, async (req, res) => {
-        const {username} = req.session;
-        const {nombre, precio, imagen, cantidad} = req.body;
+        const { username } = req.session;
+        const { nombre, precio, imagen, cantidad } = req.body;
 
-        let err = 'Los siguientes campos son requeridos: '
+        let err = 'Los siguientes campos son requeridos:'
         const camposFaltantes = []
 
         try {
             verificarCampoRequerido(nombre);
         } catch (error) {
-            camposFaltantes.push('Nombre')
+            return res.render('error', {
+                mensaje: `${err} Nombre`,
+                redirigir: '/productos'
+            })
         }
 
-        try {
-            verificarCampoRequerido(precio);
-        } catch (error) {
-            camposFaltantes.push('Precio')
-        }
+        const user = await UsuarioModel.findOne({ username });
+        const productoExistente = user.productos.find(producto => producto.nombre === nombre);
 
-        try {
-            verificarCampoRequerido(imagen);
-        } catch (error) {
-            camposFaltantes.push('Imagen')
-        }
+        if (productoExistente) {
 
-        try {
-            verificarCampoRequerido(cantidad);            
-        } catch (error) {
-            camposFaltantes.push('Cantidad')
-        }
-        
-        if(camposFaltantes.length) {
-            err = err + camposFaltantes.join(', ');
-            return res.status(StatusCodes.BAD_REQUEST).send(err);
-        }
+            const posicionDelProducto = user.productos.indexOf(productoExistente);
 
-        const user = await UsuarioModel.findOne({username});
+            if (precio) productoExistente.precio = precio
+            if (imagen) productoExistente.imagen = imagen
+            if (cantidad) productoExistente.cantidad = cantidad
 
-        user.productos.push({
-            nombre,
-            precio,
-            imagen,
-            cantidad
-        })
+            user.productos[posicionDelProducto] = productoExistente;
+
+        } else {
+
+            try {
+                verificarCampoRequerido(precio);
+            } catch (error) {
+                camposFaltantes.push('Precio')
+            }
+
+            try {
+                verificarCampoRequerido(imagen);
+            } catch (error) {
+                camposFaltantes.push('Imagen')
+            }
+
+            try {
+                verificarCampoRequerido(cantidad);
+            } catch (error) {
+                camposFaltantes.push('Cantidad')
+            }
+
+            if (camposFaltantes.length) {
+                err = err + ' ' + camposFaltantes.join(', ');
+                return res.render('error', {
+                    mensaje: err,
+                    redirigir: '/productos'
+                })
+            }
+
+            user.productos.push({
+                nombre,
+                precio,
+                imagen,
+                cantidad
+            })
+        }
 
         await user.save()
 
